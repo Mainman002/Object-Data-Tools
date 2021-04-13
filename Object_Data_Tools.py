72e3e7d2485974047e840a15002ff52ac43aab0f
Mainman002 <Mainman002@users.noreply.github.com>
2021-04-13 03:34:16 -0500

    Add panel for Material / Modifier Copy
    Added another panel for buttons that can copy Materials / Modifiers to selected objects

--- Object_Data_Tools.py
+++ Object_Data_Tools.py
@@ -6,7 +6,7 @@ bl_info = {
     "author": "Johnathan Mueller",
     "descrtion": "A panel that helps with various object data operations.",
     "blender": (2, 80, 0),
-    "version": (0, 1, 1),
+    "version": (0, 1, 2),
     "location": "View3D (ObjectMode) > Sidebar > Edit Tab",
     "warning": "",
     "category": "Object"
@@ -49,18 +49,22 @@ class OBJECT_OT_Object_Data_Make_Unique(bpy.types.Operator):
         return {'FINISHED'}
 
 
+def _mesh_name(ob):
+    if ob.name.rsplit('_',1)[-1] == 'low' or ob.name.rsplit('_',1)[-1] == 'high':
+        to_pop = '_' + ob.name.rsplit('_',1)[-1]
+        ob.name = ob.name.strip(to_pop)
+
+def _mesh_data_name(ob):
+    if ob.data.name.rsplit('_',1)[-1] == 'low' or ob.data.name.rsplit('_',1)[-1] == 'high':
+        to_pop = '_' + ob.data.name.rsplit('_',1)[-1]
+        ob.data.name = ob.data.name.strip(to_pop)
 
 
 def rename_selected(self, context, _name):
     for ob in bpy.context.selected_objects:
         if ob.type == "MESH":
-            if ob.name.rsplit('_',1)[-1] == 'low' or ob.name.rsplit('_',1)[-1] == 'high':
-                to_pop = '_' + ob.name.rsplit('_',1)[-1]
-                ob.name = ob.name.strip(to_pop)
-                
-            if ob.data.name.rsplit('_',1)[-1] == 'low' or ob.data.name.rsplit('_',1)[-1] == 'high':
-                to_pop = '_' + ob.data.name.rsplit('_',1)[-1]
-                ob.data.name = ob.data.name.strip(to_pop)
+            _mesh_name(ob)
+            _mesh_data_name(ob)
             
             ob.name = ob.name + '_' + _name
             ob.data.name = ob.data.name + '_' + _name
@@ -69,13 +73,8 @@ def rename_selected(self, context, _name):
 def rename_selected_none(self, context):
     for ob in bpy.context.selected_objects:
         if ob.type == "MESH":
-            if ob.name.rsplit('_',1)[-1] == 'low' or ob.name.rsplit('_',1)[-1] == 'high':
-                to_pop = '_' + ob.name.rsplit('_',1)[-1]
-                ob.name = ob.name.strip(to_pop)
-                
-            if ob.data.name.rsplit('_',1)[-1] == 'low' or ob.data.name.rsplit('_',1)[-1] == 'high':
-                to_pop = '_' + ob.data.name.rsplit('_',1)[-1]
-                ob.data.name = ob.data.name.strip(to_pop)
+            _mesh_name(ob)
+            _mesh_data_name(ob)
 
 
 
@@ -117,6 +116,29 @@ class OBJECT_OT_Name_Active_To_Paintable_High(bpy.types.Operator):
 
 
 
+class OBJECT_OT_Material_Active_To_Selected(bpy.types.Operator):
+    bl_idname = 'wm.object_ot_material_active_to_selected'
+    bl_label = 'Materials'
+    bl_description = 'Copy materials to other objects.'
+    bl_options = {'REGISTER'}
+        
+    def execute(self, context):
+        bpy.ops.object.material_slot_copy()
+        return {'FINISHED'}
+    
+    
+class OBJECT_OT_Modifier_Active_To_Selected(bpy.types.Operator):
+    bl_idname = 'wm.object_ot_modifier_active_to_selected'
+    bl_label = 'Modifiers'
+    bl_description = 'Copy modifiers to other objects.'
+    bl_options = {'REGISTER'}
+        
+    def execute(self, context):
+        bpy.ops.object.make_links_data(type='MODIFIERS')
+        return {'FINISHED'}
+
+
+
 
 class OBJECT_PT_Data_Panel(bpy.types.Panel):
     bl_idname = 'OBJECT_PT_data_panel'
@@ -144,6 +166,33 @@ class OBJECT_PT_Data_Panel(bpy.types.Panel):
         col = layout.column()
         
         
+
+
+
+class OBJECT_PT_Data_Copy_Panel(bpy.types.Panel):
+    bl_idname = 'OBJECT_PT_data_copy_panel'
+    bl_category = 'Object Data'
+    bl_label = 'Mesh Copy Tools'
+    bl_context = "objectmode"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+
+
+    def draw(self, context):
+        myscene = context.scene
+        layout = self.layout
+
+        if bpy.context.selected_objects:
+            row = layout.row(align=True)
+            row.operator('wm.object_ot_material_active_to_selected', icon='MATERIAL')
+            row.operator('wm.object_ot_modifier_active_to_selected', icon='MODIFIER')
+        else:
+            row = layout.row(align=True)
+            row.label(text='No objects selected')
+        
+        col = layout.column()
+
+
 
 
 
@@ -176,14 +225,19 @@ class OBJECT_PT_Data_Name_Panel(bpy.types.Panel):
 
 
 
+
+
 classes = (
     OBJECT_PT_Data_Panel,
+    OBJECT_PT_Data_Copy_Panel,
     OBJECT_PT_Data_Name_Panel,
     OBJECT_OT_Object_Data_Make_Copy,
     OBJECT_OT_Object_Data_Make_Unique,
     OBJECT_OT_Name_Active_To_Paintable_Low,
     OBJECT_OT_Name_Active_To_Paintable_High,
     OBJECT_OT_Name_Active_To_Paintable_None,
+    OBJECT_OT_Material_Active_To_Selected,
+    OBJECT_OT_Modifier_Active_To_Selected,
 )
 
 
